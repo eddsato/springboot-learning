356746b3eb5e6231e83ac818bcec5d3340f317a3
Eduardo Sato <eddsato@Eduardos-MacBook-Pro.local>
2023-02-03 03:40:24 -0300

    create user-api controller

--- user-api/src/main/java/com/eddsato/java/backend/controller/UserController.java
+++ user-api/src/main/java/com/eddsato/java/backend/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.eddsato.java.backend.controller;
 
 import com.eddsato.java.backend.model.UserDTO;
+import com.eddsato.java.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -14,72 +16,77 @@ import java.util.List;
 */
 @RestController
 public class UserController {
-    public static List<UserDTO> usuarios = new ArrayList<UserDTO>();
 
-    @GetMapping("/users")
+    /*
+        Injeção de dependencia
+     */
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/")
     public List<UserDTO> getUsers() {
+        List<UserDTO> usuarios = userService.getAll();
         return usuarios;
     }
 
-    @GetMapping("/users/{cpf}")
-    public UserDTO getUsersFiltro(@PathVariable String cpf) {
-        for (UserDTO userFilter: usuarios) {
-            if (userFilter.getCpf().equals(cpf)) {
-                return userFilter;
-            }
-        }
-        return null;
+    @GetMapping("/user/{id}")
+    UserDTO findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
-    @PostMapping("/newUser")
-    UserDTO inserir (@RequestBody UserDTO userDTO) {
-        userDTO.setDataCadastro(new Date());
-        usuarios.add(userDTO);
-        return userDTO;
+    @PostMapping("/user")
+    UserDTO newUser(@RequestBody UserDTO userDTO) {
+        return userService.save(userDTO);
     }
 
-    @DeleteMapping("/user/{cpf}")
-    public boolean remover(@PathVariable String cpf) {
-        for (UserDTO userFilter: usuarios) {
-            if (userFilter.getCpf().equals(cpf)) {
-                usuarios.remove(userFilter);
-                return true;
-            }
-        }
-        return false;
+    @GetMapping("/user/cpf/{cpf}")
+    UserDTO findByCpf(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
+    }
+
+    @DeleteMapping("/user/{id}")
+    UserDTO delete(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+
+    @GetMapping("/user/search")
+    public List<UserDTO> queryByName(
+        @RequestParam(name="nome", required = true) String nome
+    ) {
+        return userService.queryByName(nome);
     }
 
     /*
         Run right after the container initializes the class
      */
-    @PostConstruct
-    public void initiateList() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setNome("Eduardo");
-        userDTO.setCpf("123");
-        userDTO.setEndereco("Rua A");
-        userDTO.setEmail("eduardo@email.com");
-        userDTO.setTelefone("1234-3454");
-        userDTO.setDataCadastro(new Date());
-
-        UserDTO userDTO2 = new UserDTO();
-        userDTO2.setNome("Luiz");
-        userDTO2.setCpf("456");
-        userDTO2.setEndereco("Rua B");
-        userDTO2.setEmail("luiz@email.com");
-        userDTO2.setTelefone("1234-3454");
-        userDTO2.setDataCadastro(new Date());
-
-        UserDTO userDTO3 = new UserDTO();
-        userDTO3.setNome("Bruna");
-        userDTO3.setCpf("678");
-        userDTO3.setEndereco("Rua C");
-        userDTO3.setEmail("bruna@email.com");
-        userDTO3.setTelefone("1234-3454");
-        userDTO3.setDataCadastro(new Date());
-
-        usuarios.add(userDTO);
-        usuarios.add(userDTO2);
-        usuarios.add(userDTO3);
-    }
+//    @PostConstruct
+//    public void initiateList() {
+//        UserDTO userDTO = new UserDTO();
+//        userDTO.setNome("Eduardo");
+//        userDTO.setCpf("123");
+//        userDTO.setEndereco("Rua A");
+//        userDTO.setEmail("eduardo@email.com");
+//        userDTO.setTelefone("1234-3454");
+//        userDTO.setDataCadastro(new Date());
+//
+//        UserDTO userDTO2 = new UserDTO();
+//        userDTO2.setNome("Luiz");
+//        userDTO2.setCpf("456");
+//        userDTO2.setEndereco("Rua B");
+//        userDTO2.setEmail("luiz@email.com");
+//        userDTO2.setTelefone("1234-3454");
+//        userDTO2.setDataCadastro(new Date());
+//
+//        UserDTO userDTO3 = new UserDTO();
+//        userDTO3.setNome("Bruna");
+//        userDTO3.setCpf("678");
+//        userDTO3.setEndereco("Rua C");
+//        userDTO3.setEmail("bruna@email.com");
+//        userDTO3.setTelefone("1234-3454");
+//        userDTO3.setDataCadastro(new Date());
+//
+//        usuarios.add(userDTO);
+//        usuarios.add(userDTO2);
+//        usuarios.add(userDTO3);
+//    }
 }
